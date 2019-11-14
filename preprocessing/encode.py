b5cd6bdb44bc800f7510f67ccc8b4e64cfb9abb6
Evan Mullins <evancmullins@gmail.com>
2019-11-14 13:10:06 -0600

    change classifier evaluation to class

--- preprocessing/encode.py
+++ preprocessing/encode.py
@@ -1,6 +1,7 @@
 import functools
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import LabelBinarizer
 
 class TargetEncoder(BaseEstimator, TransformerMixin):
 
